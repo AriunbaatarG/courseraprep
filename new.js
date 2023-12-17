5659f8009de7144cdc0ddc177948b990dcff16e3
AriunbaatarG <g.ariunbaatar861@gmail.com>
2023-12-17 03:37:37 +0100

    js program

--- new.js
+++ new.js
@@ -1,2 +1,12 @@
 console.log("Hello World");
 //first js program
+
+console.log("%cHello World", "color: blue; font-size: 40px");
+// %c for styling with CSS
+
+console.log("Hello" + "there, " + "World");
+console.log("Hello" , "there, " , "World");
+//difference between + and ,
+//Hellothere, World
+//Hello there,  World
+
