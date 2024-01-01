1794347dc54ccd699aac8b06818b9c63af41002f
AriunbaatarG <g.ariunbaatar861@gmail.com>
2024-01-01 01:42:35 +0100

    events

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import Heading from './Heading';
-import Card from './Card';
 
+function handleClick() { 
+  let randomNum = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNum);
+  let userInput = prompt('type a number'); 
+  alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+}
 function App() {
   return (
     <div className = "App">
-      <Card h2="First card's h2" h3="First card's h3" />
-      <Card h2="Second card's h2" h3="Second card's h3" />
-      <Card h2="Third card's h2" h3="Third card's h3" />
+      <button onClick = {handleClick}>
+        Guess the number between 1 and 3
+      </button>
     </div>
   )
 }
